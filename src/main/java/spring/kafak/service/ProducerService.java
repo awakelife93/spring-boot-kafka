ec27f1f74b9349696714127e2a98bb1c62fdc0ee
harry <awakelife93@gmail.com>
2022-03-16 21:07:17 +0900

    sendMessage 반환 값 얻기 위한 ListenableFutureCallback api 추가

--- src/main/java/spring/kafak/service/ProducerService.java
+++ src/main/java/spring/kafak/service/ProducerService.java
@@ -1,7 +1,10 @@
 package spring.kafak.service;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Service
 public class ProducerService {
@@ -13,6 +16,16 @@ public class ProducerService {
   }
 
   public void sendMessage(String topic, String message) {
-    this.kafkaTemplate.send(topic, message);
+    ListenableFuture<SendResult<String, String>> listenable = this.kafkaTemplate.send(topic, message);
+    listenable.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+      @Override
+      public void onSuccess(SendResult<String, String> result) {
+      }
+
+      @Override
+      public void onFailure(Throwable ex) {
+      }
+    });
   }
 }
