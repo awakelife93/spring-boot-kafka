cdf8f4c33addfe38856f16c40ff2c5a7e7df7b8d
harry <awakelife93@gmail.com>
2022-03-16 20:46:20 +0900

    exception 제어를 위한 errorHandler 생성

---
+++ src/main/java/spring/kafak/error/errorHandler.java
@@ -0,0 +1,10 @@
+package spring.kafak.error;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@RestControllerAdvice
+public class errorHandler {
+
+}
