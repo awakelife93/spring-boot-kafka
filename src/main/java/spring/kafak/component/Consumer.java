be22388c8d86b4036a263ffbc95456383c508eb3
harry <awakelife93@gmail.com>
2022-03-18 00:01:25 +0900

    Consumer 컴포넌트 생성

---
+++ src/main/java/spring/kafak/component/Consumer.java
@@ -0,0 +1,11 @@
+package spring.kafak.component;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class Consumer {
+
+}
