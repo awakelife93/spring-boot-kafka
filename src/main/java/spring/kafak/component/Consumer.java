4b899e198b09c4362337b82fd3399a909a2180ac
harry <awakelife93@gmail.com>
2022-03-21 22:03:28 +0900

    consumer listenMessage 추가

--- src/main/java/spring/kafak/component/Consumer.java
+++ src/main/java/spring/kafak/component/Consumer.java
@@ -1,5 +1,8 @@
 package spring.kafak.component;
 
+import java.io.IOException;
+
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -8,4 +11,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class Consumer {
 
+  @KafkaListener(id = "tokenContainer", topics = "tokenStorage", groupId = "${kafka.groupId}", autoStartup = "false")
+  public void listenMessage(String message) throws IOException {
+    System.out.println("Received Message in group foo: " + message);
+  }
 }
