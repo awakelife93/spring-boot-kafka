2b85bc86c197b22fc7aaa02949b653e1d3b2bb88
harry <awakelife93@gmail.com>
2022-03-22 21:49:57 +0900

    Action Server에게 통신 요청

--- src/main/java/spring/kafak/component/Consumer.java
+++ src/main/java/spring/kafak/component/Consumer.java
@@ -1,11 +1,18 @@
 package spring.kafak.component;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.util.HashMap;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import spring.kafak.component.utils.ConvertUtils;
+import spring.kafak.component.utils.HttpUtils;
 
 @Component
 @RequiredArgsConstructor
@@ -13,7 +20,39 @@ public class Consumer {
 
   @KafkaListener(id = "tokenContainer", topics = "tokenStorage", groupId = "${kafka.groupId}", autoStartup = "false")
   public void listenMessage(String message) throws IOException {
-    System.out.println("Received Message in group foo: " + message);
-    // todo: node-sqs-message-action 프로젝트 리팩토링 & 여기서 node-sqs-message-action로 전송하기.
+    System.out.println("Received Message : " + message);
+    sendMessageToActionServer(message);
+  }
+
+  private void sendMessageToActionServer(String message) {
+    try {
+      System.out.println("Send Message To Action Server : " + message);
+
+      // todo: domain 관리 어떻게 할지 고민
+      final String actionServerAddress = "http://localhost:5005/deleteUserToken";
+      HashMap<String, Object> response = new HashMap<String, Object>();
+      HashMap<String, Object> body = new HashMap<String, Object>();
+      body.put("params", message);
+
+      String jsonParams = ConvertUtils.objectToJsonString(body);
+      HttpURLConnection request = HttpUtils.generateRequest(actionServerAddress, HttpMethod.POST.name());
+
+      DataOutputStream dataOutputStream = new DataOutputStream(request.getOutputStream());
+      dataOutputStream.write(jsonParams.getBytes("UTF-8"));
+      dataOutputStream.flush();
+      dataOutputStream.close();
+
+      response.put("status", request.getResponseCode());
+      response.put("data", HttpUtils.generateResponse(request));
+
+      System.out.println("============>" + response.get("data"));
+    } catch (UnsupportedEncodingException exception) {
+      // todo: exception 하나 만들기
+      System.out.println("UnsupportedEncodingException " + exception);
+    } catch (IOException exception) {
+      // todo: exception 하나 만들기
+      System.out.println("IOException " + exception);
+    }
+
   }
 }
