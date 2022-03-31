75837174a61418ba59ceb0fea395ab1e50b5dfbc
harry <awakelife93@gmail.com>
2022-03-31 19:07:13 +0900

    http components 추가로 인한 리팩토링

--- src/main/java/spring/kafak/component/Consumer.java
+++ src/main/java/spring/kafak/component/Consumer.java
@@ -1,56 +1,39 @@
 package spring.kafak.component;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import lombok.RequiredArgsConstructor;
-import spring.kafak.component.utils.ConvertUtils;
 import spring.kafak.component.utils.HttpUtils;
 
 @Component
 @RequiredArgsConstructor
 public class Consumer {
+  private final HttpUtils httpUtils;
 
   @KafkaListener(id = "tokenContainer", topics = "tokenStorage", groupId = "${kafka.groupId}", autoStartup = "false")
-  public void listenMessage(String message) throws IOException {
+  public void listenMessage(String message) {
     System.out.println("Received Message : " + message);
     sendMessageToActionServer(message);
   }
 
   private void sendMessageToActionServer(String message) {
-    try {
-      System.out.println("Send Message To Action Server : " + message);
+    System.out.println("Send Message To Action Server : " + message);
+    Map<String, Object> response = new HashMap<String, Object>();
 
-      // todo: domain 관리 어떻게 할지 고민
-      final String actionServerAddress = "http://localhost:5005/deleteUserToken";
-      HashMap<String, Object> response = new HashMap<String, Object>();
-      HashMap<String, Object> body = new HashMap<String, Object>();
-      body.put("params", message);
+    // todo: domain 관리 어떻게 할지 고민
+    final String actionServerAddress = "http://localhost:5005/deleteUserToken";
+    MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
 
-      String jsonParams = ConvertUtils.objectToJsonString(body);
-      HttpURLConnection request = HttpUtils.generateRequest(actionServerAddress, HttpMethod.POST.name());
+    parameters.add("params", message);
 
-      DataOutputStream dataOutputStream = new DataOutputStream(request.getOutputStream());
-      dataOutputStream.write(jsonParams.getBytes("UTF-8"));
-      dataOutputStream.flush();
-      dataOutputStream.close();
-
-      response.put("status", request.getResponseCode());
-      response.put("data", HttpUtils.generateResponse(request));
-
-      System.out.println("============>" + response.get("data"));
-    } catch (UnsupportedEncodingException exception) {
-      System.out.println("sendMessageToActionServer UnsupportedEncodingException " + exception);
-    } catch (IOException exception) {
-      System.out.println("sendMessageToActionServer IOException " + exception);
-    }
-
+    response = httpUtils.request(HttpMethod.POST, actionServerAddress, parameters);
+    System.out.println("============>" + response.get("data"));
   }
 }
