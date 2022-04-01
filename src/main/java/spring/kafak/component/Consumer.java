a6b02b728d94cc36ea379fb5f12a02ac78772edc
hw.park <hw.park@fastfive.co.kr>
2022-04-01 17:54:02 +0900

    value annotation을 통한 endpoint 주입

--- src/main/java/spring/kafak/component/Consumer.java
+++ src/main/java/spring/kafak/component/Consumer.java
@@ -3,6 +3,7 @@ package spring.kafak.component;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,9 @@ import spring.kafak.component.utils.HttpUtils;
 public class Consumer {
   private final HttpUtils httpUtils;
 
+  @Value("${endpoint.deleteUserToken}")
+  private String deleteUserTokenEndPoint;
+
   @KafkaListener(id = "tokenContainer", topics = "tokenStorage", groupId = "${kafka.groupId}", autoStartup = "false")
   public void listenMessage(String message) {
     System.out.println("Received Message : " + message);
@@ -29,14 +33,13 @@ public class Consumer {
       System.out.println("Send Message To Action Server : " + message);
       Map<String, Object> response = new HashMap<String, Object>();
 
-      // todo: domain 관리 어떻게 할지 고민
-      final String actionServerAddress = "http://localhost:5005/deleteUserToken";
       MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
 
       parameters.add("params", message);
 
-      response = httpUtils.request(HttpMethod.POST, actionServerAddress, parameters);
-      System.out.println("============>" + response.get("data"));
+      response = httpUtils.request(HttpMethod.POST, deleteUserTokenEndPoint, parameters);
+
+      System.out.println("status : " + response.get("status") + "data : " + response.get("data") );
     } catch (RestClientException exception) {
       System.out.println("RestClientException : " + exception.getMessage());
       throw exception;
