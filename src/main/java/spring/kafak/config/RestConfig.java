ee4a27a93d74b5eae3cc8070d26b998609d52825
harry <awakelife93@gmail.com>
2022-04-03 10:18:33 +0900

    RestConfig 메소드 분리

--- src/main/java/spring/kafak/config/RestConfig.java
+++ src/main/java/spring/kafak/config/RestConfig.java
@@ -10,18 +10,24 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfig {
 
-  @Bean
-  public RestTemplate restTemplate() {
-    HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-    factory.setReadTimeout(5000);
-    factory.setConnectTimeout(3000);
-    HttpClient httpClient = HttpClientBuilder.create()
+  private HttpClient generateHttpClient() {
+    return HttpClientBuilder.create()
         .setMaxConnTotal(50)
         .setMaxConnPerRoute(20).build();
-    factory.setHttpClient(httpClient);
+  }
 
-    RestTemplate restTemplate = new RestTemplate(factory);
+  private HttpComponentsClientHttpRequestFactory generateFactory() {
+    HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
 
-    return restTemplate;
+    factory.setReadTimeout(5000);
+    factory.setConnectTimeout(3000);
+    factory.setHttpClient(generateHttpClient());
+
+    return factory;
+  }
+
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate(generateFactory());
   }
 }
